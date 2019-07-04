e3347a6a9a2f255f32583ba0856e4b28b25c851e
gabrieltino <miracle8093@gmail.com>
2019-07-04 16:38:30 +0100

    spacex mern

---
+++ public/precache-manifest.1d5634888fcf4ac39dbd8e8cf0ddf31e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0f9c59c5e3d0b66f92f5b0d214c66547",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b00bcd7e1937da800f35",
+    "url": "/static/css/main.cfda8877.chunk.css"
+  },
+  {
+    "revision": "3194b33749313b8006cb",
+    "url": "/static/js/2.5259be42.chunk.js"
+  },
+  {
+    "revision": "b00bcd7e1937da800f35",
+    "url": "/static/js/main.0f923a76.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
